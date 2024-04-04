3c0f9df93be6acd8856135d99cca0a83a0447569
NNNXT <74697758+NNNXT@users.noreply.github.com>
2024-04-04 18:22:04 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/demo_comsci/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.demo_comsci
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const String route = '/';
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
